cbc9b1e65066225aee53aeeb1f7553a2b6c77bec
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-21 11:25:13 -0300

    css module

---
+++ exercicios/src/pages/css/integracao2.jsx
@@ -0,0 +1,13 @@
+import styles from './integracao2.module.css';
+
+const integracao2 = () => {
+  return (
+    <div>
+      <div className={styles.bloco1}></div>
+      <div className={styles.bloco2}></div>
+      <div className={styles.bloco3}></div>
+    </div>
+  );
+};
+
+export default integracao2;

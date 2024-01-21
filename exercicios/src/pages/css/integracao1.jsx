1f75c8670bb9e4de4d7c99253d43f9973667f185
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-21 11:10:23 -0300

    integracao css

---
+++ exercicios/src/pages/css/integracao1.jsx
@@ -0,0 +1,13 @@
+import styles from './integracao1.css';
+
+const integracao1 = () => {
+  return (
+    <div className="container">
+      <div className="vermelha">Texto 01</div>
+      <div className="azul">Texto 02</div>
+      <div className="branco">Texto 02</div>
+    </div>
+  );
+};
+
+export default integracao1;
